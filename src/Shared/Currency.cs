37565bc774043fec170fb26e862e63773f6cebd4
She-MO <nikitashp3@gmail.com>
2025-06-29 20:01:32 +0300

    Add Currency entity

---
+++ src/Shared/Currency.cs
@@ -0,0 +1,26 @@
+﻿namespace Bookify.Domain.Shared;
+
+public class Currency
+{
+    public static readonly Currency None = new("");
+    
+    public static readonly Currency Usd = new("USD");
+    
+    public static readonly Currency Eur = new("Eur");
+
+    public static readonly IReadOnlyCollection<Currency> All =
+    [
+        Usd,
+        Eur
+    ];
+        
+    public string Code { get; init; }
+
+    public Currency(string code) => Code = code;
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ??
+               throw new ApplicationException("The currency code is invalid");
+    }
+}
